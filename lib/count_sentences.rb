351249a4e80d0a56df4d056655dee2ccffb65fc0
Chloe <chloepeng331@icloud.com>
2022-03-16 00:03:45 -0400

    commit tree 2cbc75f555500ea217894a11b3e6c185e1eec15f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
+    # delimiters = [".", '!', "??"]
+    # arr = self.split(Regexp.union(delimiters))
+    # arr.count
 
+
+
+    arr = self.split(".", '!', "?")
+    # self.split(".")
+    arr.count
+
+    # arr = self.split(".", '!', "?")
+    # self.split(".")
   end
 end
